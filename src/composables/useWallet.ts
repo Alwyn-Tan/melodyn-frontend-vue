9ebef45c38bf8c3dc154c29796d71ec822b9919d
Alwyn-Tan <2660412180@qq.com>
2025-08-19 22:49:55 +0800

    add: useWallet to handle wallet connection

---
+++ src/composables/useWallet.ts
@@ -0,0 +1,71 @@
+import {reactive, ref, toValue} from 'vue';
+import {ethers} from "ethers";
+import detectEthereumProvider from "@metamask/detect-provider";
+
+interface WalletState {
+    isConnected: boolean;
+    address: string | null;
+    provider: ethers.BrowserProvider | null;
+    signer: ethers.JsonRpcSigner | null;
+    chainId: number | null;
+}
+
+export function useWallet() {
+    const wallet = reactive<WalletState>({
+        isConnected: false,
+        address: null,
+        provider: null,
+        signer: null,
+        chainId: null,
+    });
+
+    const isConnecting = ref(false);
+    const error = ref<string | null>(null);
+
+    const connectWallet = async () => {
+        try {
+            isConnecting.value = true;
+            error.value = null;
+
+            const provider = await detectEthereumProvider();
+            if (!provider) {
+                throw new Error('MetaMask not detected. Please install MetaMask')
+            }
+            const ethersProvider = new ethers.BrowserProvider(provider as any);
+            const accounts = await ethersProvider.send('eth_requestAccounts', [])
+            if (accounts.length === 0) {
+                throw new Error('No accounts found. Please connect your wallet.')
+            }
+
+            const signer = await ethersProvider.getSigner();
+            const address = await signer.getAddress();
+            const network = await ethersProvider.getNetwork();
+
+            wallet.isConnected = true;
+            wallet.address = address;
+            wallet.provider = ethersProvider;
+            wallet.signer = signer;
+            wallet.chainId = Number(network.chainId);
+        } catch (err: any) {
+            error.value = err.message || 'Failed to connect wallet';
+        } finally {
+            isConnecting.value = false;
+        }
+    }
+
+    const disconnectWallet = () => {
+        wallet.isConnected = false;
+        wallet.address = null;
+        wallet.provider = null;
+        wallet.signer = null;
+        wallet.chainId = null;
+    }
+
+    return {
+        wallet,
+        connectWallet,
+        disconnectWallet,
+        isConnecting: toValue(isConnecting),
+        error: toValue(error),
+    }
+}
